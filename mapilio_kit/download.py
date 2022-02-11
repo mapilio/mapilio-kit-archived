1f19be9c5cdfad06ba41533b8544441501076f3d
mcv_ <murat@visiosoft.com.tr>
2022-02-11 18:02:04 +0300

    download config has added

--- mapilio_kit/download.py
+++ mapilio_kit/download.py
@@ -1,17 +1,22 @@
+from .api_v1 import MAPILIO_GRAPH_API_ENDPOINT_DOWNLOAD
+from .download_config import QUALITY
+
+
 def download(
         organization_key: str,
         project_key: str,
-        download_path: str
+        download_path: str,
 ):
     """
 
     :param organization_key: your organization key, you can get your dashboard
     :param project_key: your organization key, you can get your dashboard
     :param download_path: where will be saving path
+    :param quality: which quality will be downloading
     :return:
     """
     print(organization_key)
-    print("\n")
     print(project_key)
-    print("\n")
     print(download_path)
+    print(QUALITY)
+    print(MAPILIO_GRAPH_API_ENDPOINT_DOWNLOAD)
