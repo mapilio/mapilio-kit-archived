f76394eefd5fc3bf2d3495c6233354f568e77f42
mcv_ <murat@visiosoft.com.tr>
2022-06-09 17:17:29 +0300

    uploader fixed

--- install.sh
+++ install.sh
@@ -9,18 +9,6 @@ python3 -m venv mapilio_venv
 source mapilio_venv/bin/activate
 set -e
 
-mkdir -p bin
-mkdir -p dependencies
-
-#
-# install EAC to Equirectangular conversion binary
-#
-git -C dependencies  clone https://github.com/mcvarer/max2sphere-batch
-
-make -C dependencies/max2sphere-batch -j
-
-cp dependencies/max2sphere-batch/MAX2spherebatch bin/
-
 python setup.py install --force > /dev/null
 mapilio_kit --version
 
